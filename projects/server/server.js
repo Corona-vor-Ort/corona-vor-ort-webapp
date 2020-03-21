6cbb4881e68585a452d7ea6157abc33dcd0e6f34
Nicole Broß <nicole.bross1@gmail.com>
2020-03-21 09:04:23 +0100

    Initial commit

---
+++ projects/server/server.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const compression = require('compression');
+
+const CONTEXT = `/${process.env.CONTEXT || 'wirvsvirus-staatlichekommunikation'}`;
+const PORT = process.env.PORT || 4000;
+
+const app = express();
+
+app.use(compression());
+app.use(
+  CONTEXT,
+  express.static(
+    path.resolve(__dirname, '../../dist/wirvsvirus-staatlichekommunikation')
+  )
+);
+app.use(
+  '/',
+  express.static(
+    path.resolve(__dirname, '../../dist/wirvsvirus-staatlichekommunikation')
+  )
+);
+app.listen(PORT, () =>
+  console.log(`App running on http://localhost:${PORT}${CONTEXT}`)
+);
